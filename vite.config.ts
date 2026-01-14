646f07982d9d5f64f15a24daad1718731e4a4a93
NEKOgrile <willemcornil@gmail.com>
2026-01-14 16:35:50 +0100

    test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base is required for GitHub Pages hosting at https://nekogrile.github.io/taxi-nekogrile/
+  base: '/taxi-nekogrile/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
