eb4d15c121cdc52fe6dbad2270f5a60ebf8f03c0
NEKOgrile <willemcornil@gmail.com>
2026-01-14 16:44:39 +0100

    Add GH Pages deploy: scripts and workflow, set Vite base

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base is required for GitHub Pages hosting at https://nekogrile.github.io/taxi-nekogrile/
-  base: '/taxi-nekogrile/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: '/taxi-patrice/',
+
 });
